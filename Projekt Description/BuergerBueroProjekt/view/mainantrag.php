43be9953dee93b98cf7e98b537d39a4f9f4c3d4f
HollywoodHankK <163832209+HollywoodHankK@users.noreply.github.com>
2024-03-20 14:10:57 +0100

    create Anträge PHP Javascript CSS
    PHP Javascript CSS HTML

---
+++ Projekt Description/BuergerBueroProjekt/view/mainantrag.php
@@ -0,0 +1,32 @@
+<main>
+    <section class="container">
+        <h1>Anträge</h1>
+        <div>
+            <div>
+                <p>
+                    Hier finden sie verschiedenste Anträge zu Ihren persönlichen Anliegen
+                </p>
+            </div>
+            <div>
+                <label for="abteilung">Wählen sie die passende Behörde aus</label>
+                <select id="abteilung" onchange="handleSelection()"></select>
+                <div id="antr-list">
+                    <h3 id="grupe-name">Führerscheinstelle</h3>
+                    <div class="elements-container">
+                        <ul id="elements" class="antrag-list">
+                            <li class="antrag-element">
+                                <span>Antrag auf Fahreignung</span>
+                                <a href="#">Klicken Sie hier...</a>
+                            </li>
+                            <li class="antrag-element">
+                                <span>Antrag auf Fahreignung</span>
+                                <a href="#">Klicken Sie hier...</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <hr />
+            </div>
+    </section>
+</main>
---
+++ Projekt Description/BuergerBueroProjekt/view/scripte/antrag.js
@@ -0,0 +1,82 @@
+const antrageList = fetch("./../model/AnträgeMockup.json").then((response) =>
+  response.json()
+);
+
+function populateSelect() {
+  antrageList
+    .then((data) => {
+      const uniqueGruppen = [...new Set(data.map((item) => item.Gruppe))];
+
+      const gruppeNameElement = document.getElementById("grupe-name");
+      const select = document.getElementById("abteilung");
+      uniqueGruppen.forEach((gruppe) => {
+        const option = document.createElement("option");
+        option.value = gruppe;
+        option.textContent = gruppe;
+        select.appendChild(option);
+      });
+      gruppeNameElement.innerText = select.value;
+
+      const selectedElements = data.filter(
+        (item) => item.Gruppe === select.value
+      );
+
+      const container = document.getElementById("elements");
+      container.innerHTML = "";
+
+      selectedElements.forEach((item) => {
+        const listItem = document.createElement("li");
+        listItem.classList.add("antrag-element");
+
+        const span = document.createElement("span");
+        span.textContent = item.Gruppe;
+
+        const link = document.createElement("a");
+        link.href = item.Link;
+        link.textContent = "Klicken Sie hier...";
+        link.target = "_blank";
+
+        listItem.appendChild(span);
+        listItem.appendChild(link);
+        container.appendChild(listItem);
+      });
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+window.onload = populateSelect;
+
+function handleSelection() {
+  const selectElement = document.getElementById("abteilung");
+  const gruppeNameElement = document.getElementById("grupe-name");
+
+  gruppeNameElement.innerText = selectElement.value;
+
+  const container = document.getElementById("elements");
+  container.innerHTML = "";
+
+  antrageList
+    .then((data) => {
+      const selectedElements = data.filter(
+        (item) => item.Gruppe === selectElement.value
+      );
+
+      selectedElements.forEach((item) => {
+        const listItem = document.createElement("li");
+        listItem.classList.add("antrag-element");
+
+        const span = document.createElement("span");
+        span.textContent = item.Gruppe;
+
+        const link = document.createElement("a");
+        link.href = item.Link;
+        link.textContent = "Klicken Sie hier...";
+        link.target = "_blank";
+
+        listItem.appendChild(span);
+        listItem.appendChild(link);
+        container.appendChild(listItem);
+      });
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
