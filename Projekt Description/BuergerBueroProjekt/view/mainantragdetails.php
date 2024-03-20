5a55eae273953ecebdddb0555cd09324dc1aad3e
HollywoodHankK <163832209+HollywoodHankK@users.noreply.github.com>
2024-03-20 14:33:35 +0100

    update Antrag
    add Antrag Details HTML

--- Projekt Description/BuergerBueroProjekt/view/mainantragdetails.php
+++ Projekt Description/BuergerBueroProjekt/view/mainantragdetails.php
@@ -1,47 +1,25 @@
 <main>
-    <div class="container">
-        <h1>Anträge</h1>
+    <section>
         <div>
-            <h3>Führerscheinstelle</h3>
+            <h1>
+                Title
+            </h1>
+            <p>
+                Description
+            </p>
             <div>
-                <ul class="antrag-list">
-                    <li class="antrag-element">
-                        <span>Antrag auf Fahreignung</span>
-                        <a href="#" class="button">Klicken Sie hier...</a>
-                    </li>
-                    <li class="antrag-element">
-                        <span>Antrag auf Fahreignung</span>
-                        <a href="#" class="button">Klicken Sie hier...</a>
-                    </li>
-                </ul>
+                <p>
+                    Content
+                </p>
             </div>
-            <hr>
-            <h3>Ausländeramt</h3>
             <div>
-                <ul class="antrag-list">
-                    <li class="antrag-element">
-                        <span>Antrag auf Duldung</span>
-                        <a href="#" class="button">Klicken Sie hier...</a>
-                    </li>
-                    <li class="antrag-element">
-                        <span>Antrag auf Niederlassungserlaubniss</span>
-                        <a href="#" class="button">Klicken Sie hier...</a>
-                    </li>
-                </ul>
-            </div>
-            <hr>
-            <h3>Einbürgerungsbehörde</h3>
-            <div>
-                <ul class="antrag-list">
-                    <li class="antrag-element">
-                        <span>Antrag auf Loyalitätserlaubniss</span>
-                        <a href="#" class="button">Klicken Sie hier...</a>
-                    </li>
-                    <li class="antrag-element">
-                        <span>Antrag auf Einbürgerung</span>
-                        <a href="#" class="button">Klicken Sie hier...</a>
-                    </li>
-                </ul>
+                <button>
+                    Zurück
+                </button>
+                <button>
+                    Senden
+                </button>
             </div>
         </div>
+    </section>
 </main>
