128faceba68979593908602d8a83f12fa92a17aa
HollywoodHankK <163832209+HollywoodHankK@users.noreply.github.com>
2024-03-20 17:27:59 +0100

    update Anträge
    BugFix

--- Projekt Description/BuergerBueroProjekt/view/mainantragdetails.php
+++ Projekt Description/BuergerBueroProjekt/view/mainantragdetails.php
@@ -1,49 +1,4 @@
 <main>
-    <header>
-        <h1 class="title">
-            Title
-        </h1>
-    </header>
-    <style>
-    .container {
-        margin: 20px;
-    }
-
-    .title {
-        font-size: 24px;
-        font-weight: bold;
-    }
-
-    .description,
-    .content {
-        margin-bottom: 10px;
-    }
-
-    .button-container {
-        display: flex;
-        justify-content: center;
-        margin: 20px;
-    }
-
-    .button {
-        padding: 8px 16px;
-        background-color: #007bff;
-        color: #fff;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-        transition: background-color 0.3s ease-in-out;
-    }
-
-    .button+.button {
-        margin-left: 10px;
-    }
-
-    .button:hover {
-        background-color: #0056b3;
-    }
-    </style>
-
     <section>
         <div class="container">
             <div class="description">
