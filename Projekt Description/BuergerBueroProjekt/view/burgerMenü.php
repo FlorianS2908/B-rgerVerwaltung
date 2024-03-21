3882b5589a0555c199f0883e6429c261560c7d01
FlorianS2908 <152385409+FlorianS2908@users.noreply.github.com>
2024-03-21 10:49:59 +0100

    update

--- Projekt Description/BuergerBueroProjekt/view/burgerMenü.php
+++ Projekt Description/BuergerBueroProjekt/view/burgerMenü.php
@@ -2,7 +2,7 @@
 
 <!-- Das Dropdown-Menü -->
 <div class="dropdown" id="menuDropdown">
-    <div class="dropdown-item" onclick="window.location.href='persdata.php';">Persönliche
+    <div class="dropdown-item" onclick="window.location.href='personalpage.php';">Persönliche
         Daten</div>
     <div class="dropdown-item" onclick="window.location.href='anträge.php';">Anträge</div>
     <div class="dropdown-item" onclick="window.location.href='artikel.php';">Artikel</div>
@@ -11,22 +11,22 @@
 </div>
 
 <script>
-// JavaScript, um das Dropdown-Menü anzuzeigen/verbergen
-const menuIcon = document.getElementById("menuIcon");
-const menuDropdown = document.getElementById("menuDropdown");
+    // JavaScript, um das Dropdown-Menü anzuzeigen/verbergen
+    const menuIcon = document.getElementById("menuIcon");
+    const menuDropdown = document.getElementById("menuDropdown");
 
-menuIcon.addEventListener("click", function() {
-    if (menuDropdown.style.display === "block") {
-        menuDropdown.style.display = "none";
-    } else {
-        menuDropdown.style.display = "block";
-    }
-});
+    menuIcon.addEventListener("click", function() {
+        if (menuDropdown.style.display === "block") {
+            menuDropdown.style.display = "none";
+        } else {
+            menuDropdown.style.display = "block";
+        }
+    });
 
-// Schließen des Dropdown-Menüs, wenn außerhalb geklickt wird
-window.addEventListener("click", function(event) {
-    if (event.target !== menuIcon && event.target !== menuDropdown) {
-        menuDropdown.style.display = "none";
-    }
-});
+    // Schließen des Dropdown-Menüs, wenn außerhalb geklickt wird
+    window.addEventListener("click", function(event) {
+        if (event.target !== menuIcon && event.target !== menuDropdown) {
+            menuDropdown.style.display = "none";
+        }
+    });
 </script>
