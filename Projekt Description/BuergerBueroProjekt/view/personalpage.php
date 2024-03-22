683fb47620c2f2032924d14b097a75aa9f88b2b0
r0b1n-git <rr0b1n@tutanota.com>
2024-03-22 11:25:32 +0100

    update

--- Projekt Description/BuergerBueroProjekt/view/personalpage.php
+++ Projekt Description/BuergerBueroProjekt/view/personalpage.php
@@ -10,70 +10,74 @@
     <link rel="stylesheet" href="style/burgerMenü.css">
     <link rel="stylesheet" href="style/navigation.css">
     <link rel="stylesheet" href="style/header.css">
+    <link rel="stylesheet" href="style/main.css">
 </head>
 
 <header>
     <h1>Persönliche Daten</h1>
 </header>
+<?php
+    require "navi.php";
+    ?>
 
 <body>
-    <?php require 'burgerMenü.php'; ?>
+    <?php
+    require "burgerMenü.php";
+    ?>
+    <div class=container>
+        <div class="main">
 
-    <div class="container">
 
-        <?php require 'navi.php'; ?>
-        <br>
-
-        <p class="subtext">Hier finden Sie Ihre persönlichen Daten</p>
-        <?php
+            <p class="subtext">Hier finden Sie Ihre persönlichen Daten</p>
+            <?php
         session_start();
 
         ?>
-        <div class="fixed-fields">
-            <div class="box">
-                <div class="umrandung">
-                    <label class="titel" for="state">Nachname:</label>
-                    <p class="text nachname" name="Nachname">Mustermann</p>
+            <div class="fixed-fields">
+                <div class="box">
+                    <div class="umrandung">
+                        <label class="titel" for="state">Nachname:</label>
+                        <p class="text nachname" name="Nachname">Mustermann</p>
+                    </div>
+                    <div class="umrandung">
+                        <label class="titel" for="state">Geburtsdatum:</label>
+                        <p class="text geburtsdatum ">1990-01-01</p>
+                    </div>
                 </div>
-                <div class="umrandung">
-                    <label class="titel" for="state">Geburtsdatum:</label>
-                    <p class="text geburtsdatum ">1990-01-01</p>
+                <div class="box">
+                    <div class="umrandung">
+                        <label class="titel" for="state">Vorname:</label>
+                        <p class="text vorname">Erika</p>
+                    </div>
+                    <div class="umrandung">
+                        <label class="titel" for="state">Geburtsort:</label>
+                        <p class="text geburtsort">Berlin</p>
+                    </div>
                 </div>
             </div>
-            <div class="box">
-                <div class="umrandung">
-                    <label class="titel" for="state">Vorname:</label>
-                    <p class="text vorname">Erika</p>
-                </div>
-                <div class="umrandung">
-                    <label class="titel" for="state">Geburtsort:</label>
-                    <p class="text geburtsort">Berlin</p>
-                </div>
-            </div>
-        </div>
 
 
-        <h1 class="title2">Adresse</h1>
-        <div class="editable-fields">
-            <div class="box">
-                <div class="adresse">
-                    <label class="titel" for="state">Straße:</label>
-                    <input type="text" id="Straße" name="Straße" required value="Ihre Straße" />
-                    <label class="titel" for="address">Hausnummer:</label>
-                    <input type="text" id="Hausnummer" name="Hausnummer" required value="Ihre Hausnummer" />
-                    <label class="titel" for="zipCode">Postleitzahl:</label>
-                    <input type="text" id="PLZ" name="PLZ" required value="Ihre Postleitzahl" />
-                    <label class="titel" for="city">Stadt:</label>
-                    <input type="text" id="Ort" name="Ort" required value="Ihre Stadt" />
-                    <div class="btn-container">
-                        <button class="btn" onclick="saveData()">Speichern</button>
+            <h1 class="title2">Adresse</h1>
+            <div class="editable-fields">
+                <div class="box">
+                    <div class="adresse">
+                        <label class="titel" for="state">Straße:</label>
+                        <input type="text" id="Straße" name="Straße" required value="Ihre Straße" />
+                        <label class="titel" for="address">Hausnummer:</label>
+                        <input type="text" id="Hausnummer" name="Hausnummer" required value="Ihre Hausnummer" />
+                        <label class="titel" for="zipCode">Postleitzahl:</label>
+                        <input type="text" id="PLZ" name="PLZ" required value="Ihre Postleitzahl" />
+                        <label class="titel" for="city">Stadt:</label>
+                        <input type="text" id="Ort" name="Ort" required value="Ihre Stadt" />
+                        <div class="btn-container">
+                            <button class="btn" onclick="saveData()">Speichern</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <?php require 'footer.php'; ?>
-    <script src="scripte/personalpage.js"></script>
+        <?php require 'footer.php'; ?>
+        <script src="scripte/personalpage.js"></script>
 </body>
 
 </html>
